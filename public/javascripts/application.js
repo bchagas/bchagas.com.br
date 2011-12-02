3eaa781c751375c0af2afdf6593e629f9cb0aa63
Bruno Chagas <brunochagasweb@gmail.com>
2011-12-02 16:31:57 -0200

    Update format post time

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,8 @@
 (function(){
 
   if($('time').length >= 1) {
-    date = $('time').html(), elem = date.split('/'), day = '<span>' + elem[0] + '</span>', month = '<span>' + elem[1] + '</span>', year = '<span>' + elem[1] + '</span>';
-
-    $('header time').html(day + month + year);
-    $('aside time').html(day + month);
+    date = $('time').html(), elem = date.split('/'), day = '<span>' + elem[0] + '</span>', month = '<span>' + elem[1] + '</span>';
+    $('time').html(day + month);
   };
 
 }).call(this);
