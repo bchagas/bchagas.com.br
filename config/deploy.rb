bf73964f81886905a14c4f29d0a602479abf4a76
odelbos <od@phibox.com>
2011-12-03 22:54:26 +0100

    Remove inploy configuration

--- config/deploy.rb
+++
@@ -1,7 +0,0 @@
-application    = 'evinicius'
-repository     = 'git@github.com:emersonvinicius/evinicius.com.git'
-
-hosts        = %w(67.23.242.87)
-environment  = 'production'
-ssh_opts     = '-A'
-branch       = 'master'
