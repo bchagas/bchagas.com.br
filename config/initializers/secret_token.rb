ec7934531b4e660fbf4aabc4db0f434a6f9e9c85
odelbos <od@phibox.com>
2011-12-04 01:44:46 +0100

    Update initializer to Rails 3.1.3

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'f7822aa41517838c36a456a349e27885db7ef79aa5f8973720662019709afd8d6f9e6579341802098d7ad03b0ca2651115e4739c2b00345890ef58640218dc91'
+Themis::Application.config.secret_token = '16f276839f75c8388225fefe1bf87bfce30cb7d7f53568ac26aed6b3019a51f33a2c3c73266c5e1834affae30e356486ba0ff2406db0c0ade9196f9ae3873b6f'
