2c573144d82d38ff19f4382eb0d60b211c874f74
Bruno Chagas <brunochagasweb@gmail.com>
2011-12-05 01:33:10 -0200

    Add compass load paths

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,8 @@ module Themis
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.sass.load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
+    config.sass.load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/blueprint/stylesheets"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
