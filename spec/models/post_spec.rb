d420cde9ea56b3f4141427e2c81fb895d6216f47
Bruno Chagas <brunochagasweb@gmail.com>
2011-12-04 22:38:37 -0200

    Fix tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe Post do
   should_belong_to :author
-  should_validate_presence_of :title, :body, :author
+  should_validate_presence_of :title
+  should_validate_presence_of :body
+  should_validate_presence_of :author
   should_have_an_slugged_id :title
   should_have_default_state :saved
   should_change :from => :saved, :to => :published, :on => :publish
