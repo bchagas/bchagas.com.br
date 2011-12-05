59377a9d508bc16d4b2605f9bf833a09f41c93b9
Bruno Chagas <brunochagasweb@gmail.com>
2011-12-05 01:32:52 -0200

    Removing ldap configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_many :posts
   validates_presence_of :name
-  devise :recoverable, :rememberable, :trackable
-  Themis::Config['ldap'] ? devise(:ldap_authenticatable) : devise(:database_authenticatable)
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable
 end
