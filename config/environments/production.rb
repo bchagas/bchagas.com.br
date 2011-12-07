a4ced33b8386ba0b6c35b2e997eedc784ad86df3
odelbos <od@phibox.com>
2011-12-07 01:18:00 +0100

    Update assets host for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Themis::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = 'assets.bchagas.phibox.com'
+  config.action_controller.asset_host = 'assets.bchagas.com.br'
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
